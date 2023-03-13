38aae5510e182114cbbb33acbfc1223156d617a0
syedatehreem2002 <118798168+syedatehreem2002@users.noreply.github.com>
2023-03-13 23:11:46 +0500

    done

---
+++ Practices/Website_layout/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".image-slider img").forEach(images => {
+    images.onclick = () =>{
+        var src = images.getAttribute("src");
+        document.querySelector(".main-home-image").src = src;
+    };
+});
