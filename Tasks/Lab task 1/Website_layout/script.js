bd85a39b06519a6727b0037515105dbf5524e3ce
syedatehreem2002 <118798168+syedatehreem2002@users.noreply.github.com>
2023-04-13 12:34:46 +0500

    done

--- Tasks/Lab task 1/Website_layout/script.js
+++ Tasks/Lab task 1/Website_layout/script.js
@@ -7,3 +7,37 @@ document.querySelectorAll(".image-slider > img").forEach(images => {
     };
 });
 
+var swiper = new Swiper(".review-slider",{
+    spaceBetween: 20,
+    pagination:{
+        el:".swiper-pagination",
+        clickable: true
+    },
+    loop:true,
+    grabCursor: true,
+    autoplay:{
+        delay : 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints : {
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:1
+
+        },
+    },
+});
+
+let menu = document.querySelector('#menu_btn');
+let NavBar = document.querySelector('.NavBar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    NavBar.classList.toggle('active');
+}
+Window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    NavBar.classList.remove('active');
+}
