88c635bbe526055fc6938f601b70786c70a49583
syedatehreem2002 <118798168+syedatehreem2002@users.noreply.github.com>
2023-06-08 16:23:19 +0500

    paper done

--- WEB_Lab_Final/Final/routes/users.js
+++ WEB_Lab_Final/Final/routes/users.js
@@ -2,28 +2,75 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-
-//api/users Get
+// GET 
 router.get('/', async (req, res) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (err) {
-      console.error('Failed to fetch users:', err);
-      res.status(500).json({ error: 'Failed to fetch users' });
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error('Failed to fetch users:', err);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+// GET specific user
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
-// POST /api/users  new user
+    res.json(user);
+  } catch (err) {
+    console.error('Failed to fetch user:', err);
+    res.status(500).json({ error: 'Failed to fetch user' });
+  }
+});
+
+// POST 
 router.post('/', async (req, res) => {
-    const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password });
-    try {
-      const user = await newUser.save();
-      res.status(201).json(user);
-    } catch (err) {
-      console.error('Failed to create user:', err);
-      res.status(500).json({ error: 'Failed to create user' });
+  const { name, email, password } = req.body;
+  const newUser = new User({ name, email, password });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    console.error('Failed to create user:', err);
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+});
+
+//  update
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(id, { name, email, password }, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
-//run this on browser the api will be shown  http://localhost:3000/users
+    res.json(user);
+  } catch (err) {
+    console.error('Failed to update user:', err);
+    res.status(500).json({ error: 'Failed to update user' });
+  }
+});
+
+// DELETE 
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.sendStatus(204);
+  } catch (err) {
+    console.error('Failed to delete user:', err);
+    res.status(500).json({ error: 'Failed to delete user' });
+  }
+});
+// http://localhost:3000/users
+// Export the router
 module.exports = router;
